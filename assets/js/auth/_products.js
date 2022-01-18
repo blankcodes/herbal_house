a0020f5d06513cec2ef83c6bb4fb0bd920dde0db
blankcodes <me@kenkarlo.com>
2022-01-18 23:41:23 +0800

    change ajax call to fetch api

--- assets/js/auth/_products.js
+++ assets/js/auth/_products.js
@@ -29,137 +29,113 @@ var $w = $(window).scroll(function(){
 });
 function showProductData(page_no, nonce) {
 	$("#loader").removeAttr('hidden');
-	$.ajax({
-		url: base_url+'api/v1/product/_get_products',
-		type: 'GET',
-		dataType: 'JSON',
-        data: {page_no:page_no,nonce:nonce}
-	})
-	.done(function(res) {
-		$('#products_pagination').html(res.pagination);
-		mob_string = '';
-		string = '';
-		if (parseInt(res.count) > 0) {
-			for(var i in res.result) {
-				string += '<div class="col-md-4 col-lg-3 col-6">'
-                       +'<div class="card">'
-                           	+'<a href="'+res.result[i].url+'"><img src="'+res.result[i].product_image+'" class="card-img-top" alt="'+res.result[i].product_name+'"></a>'
-                           	+'<div class="card-body card-title-div">'
-                                +'<a href="'+res.result[i].url+'"><h2 class="card-title text-secondary product-name">'+res.result[i].product_name+'<br>'
-                                	+'<small class="product-category">'+res.result[i].category+'</small></h2>'
-                                +'</a>'
+    let params = new URLSearchParams({page_no:page_no,nonce:nonce});
+    fetch(base_url+'api/v1/product/_get_products?' + params, {
+        method: "GET",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+    })
+    .then(response => response.json())
+    .then(res => {
+        $('#products_pagination').html(res.pagination);
+        mob_string = '';
+        string = '';
+        if (parseInt(res.count) > 0) {
+            for(var i in res.result) {
+                string += '<div class="col-md-4 col-lg-3 col-6">'
+                       +'<div class="card pointer-cursor" onclick="_accessPage(\''+res.result[i].url+'\')">'
+                            +'<img src="'+res.result[i].product_image+'" class="card-img-top" alt="'+res.result[i].product_name+'">'
+                            +'<div class="card-body card-title-div">'
+                                +'<span href="'+res.result[i].url+'"><h2 class="card-title text-secondary product-name">'+res.result[i].product_name+'<br>'
+                                    +'<small class="product-category">'+res.result[i].category+'</small></h2>'
+                                +'</span>'
                                 +'<h3 class="card-title text-success">₱ '+res.result[i].price+'</h3>'
                                 // +'<button href="#add_to_cart" class="btn btn-success btn-sm mt-2" onclick="addToCart(\''+res.result[i].p_pub_id+'\')"><i class="uil-shopping-cart-alt me-1"></i> Add to cart</button>'
                             +'</div> '
                         +'</div> '
                 +'</div> '
-			}
+            }
 
-			for(var i in res.result) {
-				mob_string += '<div class="col-md-6 col-12">'
-                        +'<div class="card product-cards">'
+            for(var i in res.result) {
+                mob_string += '<div class="col-md-6 col-12">'
+                        +'<div class="card product-cards pointer-cursor" >'
                             +'<div class="row">'
                                 +'<div class="col-4 product-cards-img">'
-                                	+'<a href="'+res.result[i].url+'">'
-                                    	+'<img src="'+res.result[i].product_image+'" class="card-img-top" alt="'+res.result[i].product_name+'">'
-                                    +'</a>'
+                                    +'<span onclick="_accessPage(\''+res.result[i].url+'\')">'
+                                        +'<img src="'+res.result[i].product_image+'" class="card-img-top" alt="'+res.result[i].product_name+'">'
+                                    +'</span>'
                                 +'</div>' 
                                 +'<div class="card-body col-8">'
-                                    +'<a href="'+res.result[i].url+'">'
+                                    +'<span onclick="_accessPage(\''+res.result[i].url+'\')">'
                                         +'<h2 class="card-title text-secondary">'+res.result[i].product_name+'<br>'
                                         +'<small class="product-category">'+res.result[i].category+'</small>'
                                         +'</h2>'
-                                    +'</a>'
+                                    +'</span>'
                                     +'<h3 class=" text-success cart-product-price">₱ '+res.result[i].price+' <button class="btn btn-success btn-sm btn-rounded mobile-add-to-cart-btn" onclick="addToCart(\''+res.result[i].p_pub_id+'\')"><i class="uil-shopping-trolley "></i> Add to cart</button></h3>'
                                 +'</div> '
                             +'</div>'
                        +'</div> '
                     +'</div>'
             }
-		}
+        }
         else{
-            $('#err_title').text('Error Getting Products!')
-            $('#err_message').html("There's an error getting Products! Please refresh the page or click the <b>Refresh</b> button below.")
-            $("#_product_warning_modal").modal('show');
             string = "<div class='text-center text-secondary'>Seems there's an error. Please try again!</div>"
             mob_string = "<div class='text-center text-secondary'>Seems there's an error. Please try again!</div>"
         }
 
         $("#products_wrapper_home").html(string);
         $(".mobile_products_wrapper").html(mob_string);
-	})
-	.fail(function() {
-		
-	})
-	.always(function() {
-		$("#loader").attr('hidden','hidden');
-	});
+        $("#loader").attr('hidden','hidden');
+
+    })
+    .catch((error) => {
+        $("#loader").attr('hidden','hidden');
+        console.error('Error:', error);
+    });
 	
 }
 function showProductDataCat(page_no, nonce) {
     $("#_category_wrapper").html('<div class="mt-1 mb-1 text-center">Getting List...</div>');
     $("#_mob_category_wrapper").html('<div class="mt-1 mb-1 text-center">Getting List...</div>');
-    $.ajax({
-        url: base_url+'api/v1/product/_get_product_category_home',
-        type: 'GET',
-        dataType: 'JSON',
-        data: {page_no:page_no,nonce:nonce}
+
+    let params = new URLSearchParams({page_no:page_no,nonce:nonce});
+    fetch(base_url+'api/v1/product/_get_product_category_home?' + params, {
+        method: "GET",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
     })
-    .done(function(res) {
-        $('#products_pagination').html(res.pagination);
+    .then(response => response.json())
+    .then(res => {
         mob_string = '';
         string = '';
         if (parseInt(res.count) > 0) {
             for(var i in res.result) {
                 string += ' <div class="col-md-6 col-lg-3 col-6">'
-                        +'<div class="card">'
+                        +'<div class="card pointer-cursor" onclick="_accessPage(\''+base_url+'product/category/'+res.result[i].category_url+'\')">'
                             +'<img src="'+base_url+''+res.result[i].image+'" class="card-img-top" alt="'+res.result[i].name+'">'
                             +'<div class="card-body">'
-                                +'<a href="'+base_url+'product/category/'+res.result[i].category_url+'" class="text-success product-category-btn fw-600 stretched-link">'+res.result[i].name+' <i class="uil-angle-right "></i></a>'
+                                +'<span class="text-success product-category-btn fw-600 stretched-link">'+res.result[i].name+' <i class="uil-angle-right "></i></span>'
                             +'</div> '
                         +'</div> '
                     +'</div>'
             }
-
-            // for(var i in res.result) {
-            //     mob_string += '<div class="col-md-6 col-12">'
-            //             +'<div class="card product-cards">'
-            //                 +'<div class="row">'
-            //                     +'<div class="col-4 product-cards-img">'
-            //                         +'<a href="'+res.result[i].url+'">'
-            //                             +'<img src="'+res.result[i].product_image+'" class="card-img-top" alt="'+res.result[i].product_name+'">'
-            //                         +'</a>'
-            //                     +'</div>' 
-            //                     +'<div class="card-body col-8">'
-            //                         +'<a href="'+res.result[i].url+'">'
-            //                             +'<h2 class="card-title text-secondary">'+res.result[i].product_name+'<br>'
-            //                             +'<small class="product-category">'+res.result[i].category+'</small>'
-            //                             +'</h2>'
-            //                         +'</a>'
-            //                         +'<h3 class=" text-success cart-product-price">₱ '+res.result[i].price+' <button class="btn btn-success btn-sm btn-rounded mobile-add-to-cart-btn" onclick="addToCart(\''+res.result[i].p_pub_id+'\')"><i class="uil-shopping-trolley "></i> Add to cart</button></h3>'
-            //                     +'</div> '
-            //                 +'</div>'
-            //            +'</div> '
-            //         +'</div>'
-            // }
         }
         else{
-            $('#err_title').text('Error Getting Products!')
-            $('#err_message').html("There's an error getting Products! Please refresh the page or click the <b>Refresh</b> button below.")
-            $("#_product_warning_modal").modal('show');
             string = "<div class='text-center text-secondary'>Seems there's an error. Please try again!</div>"
             mob_string = "<div class='text-center text-secondary'>Seems there's an error. Please try again!</div>"
         }
 
         $("#_category_wrapper").html(string);
         $("#_mob_category_wrapper").html(string);
-        // $(".mobile_products_wrapper").html(mob_string);
-    })
-    .fail(function() {
-        
-    })
-    .always(function() {
         $("#loader").attr('hidden','hidden');
+
+    })
+    .catch((error) => {
+        $("#loader").attr('hidden','hidden');
+        console.error('Error:', error);
     });
-    
 }
